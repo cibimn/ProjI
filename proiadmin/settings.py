7f42e60aa61cffc9580e657b14ef53576c62cca7
Manivannan Nagarajan <cibimn@gmail.com>
2022-12-30 20:43:26 +0530

    updated settings

--- proiadmin/settings.py
+++ proiadmin/settings.py
@@ -138,7 +138,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
 LANGUAGE_CODE = 'en-us'
 
