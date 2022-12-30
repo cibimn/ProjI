28bac37e56fb79ca0ae22b4caa033c6c52b03836
Manivannan Nagarajan <cibimn@gmail.com>
2022-12-30 20:05:43 +0530

    upadte

--- proiadmin/settings.py
+++ proiadmin/settings.py
@@ -165,7 +165,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = "static_root"
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field asdas
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
